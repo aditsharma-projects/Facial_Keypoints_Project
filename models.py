7806555ef2c78e98e5a42efabbc50adcc5913573
aditsharma-proejcts <ben10raceagainsttime@gmail.com>
2020-11-04 22:26:05 -0800

    Model 1

--- models.py
+++ models.py
@@ -19,15 +19,27 @@ class Net(nn.Module):
 
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # self.conv1 = nn.Conv2d(1, 32, 5)
+        # self.conv2 = nn.Conv2d(32, 64, 3)
+        # self.conv3 = nn.Conv2d(64, 128, 1)
+        #
+        # self.pool = nn.MaxPool2d(2)
+        #
+        #
+        # self.fc1 = nn.Linear(93312, 10000)
+        # self.fc2 = nn.Linear(10000, 1000)
+        # self.fc3 = nn.Linear(1000, 136)
+
         self.conv1 = nn.Conv2d(1, 32, 5)
         self.conv2 = nn.Conv2d(32, 64, 3)
-        self.conv3 = nn.Conv2d(64, 128, 1)
+        self.conv3 = nn.Conv2d(64, 128, 5)
 
+        self.drop1 = nn.Dropout(p=0.2)
+        self.drop2 = nn.Dropout(p=0.3)
         self.pool = nn.MaxPool2d(2)
-        
 
-        self.fc1 = nn.Linear(93312, 10000)
-        self.fc2 = nn.Linear(10000, 1000)
+        self.fc1 = nn.Linear(21632, 7000)
+        self.fc2 = nn.Linear(7000, 1000)
         self.fc3 = nn.Linear(1000, 136)
 
         ## Note that among the layers to add, consider including:
@@ -38,15 +50,14 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
 
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+         x = self.pool(F.relu(self.conv1(x)))
+         x = self.pool(F.relu(self.conv2(x)))
+         x = self.pool(F.relu(self.conv3(x)))
+        #
+         x = x.view(x.size()[0], -1)
+        #
+         x = self.drop1(F.relu(self.fc1(x)))
+         x = self.drop2(F.relu(self.fc2(x)))
+         x = self.fc3(x)
 
-        x = x.view(x.size()[0], -1)
-
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-
-        # a modified x, having gone through all the layers of your model, should be returned
-        return x
+         return x
